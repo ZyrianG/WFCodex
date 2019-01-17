97e6db76073015780d90b5d258d121ce85671d99
Zyrian Gantuangco <g.zyrian@gmail.com>
2019-01-17 16:18:14 -0500

    Started making nested menus with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+const modules = [
+  MatMenuModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +20,12 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    modules,
+  ],
+  exports: [
+    modules,
   ],
   providers: [],
   bootstrap: [AppComponent]
