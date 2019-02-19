1e2183da6c170f780fb637765485ce8d75ca202f
Zyrian Gantuangco <g.zyrian@gmail.com>
2019-02-19 17:04:55 -0500

    Updated warframescontroller so it doesn't have hard coded data

--- controllers/WarframesController.js
+++ controllers/WarframesController.js
@@ -3,9 +3,10 @@ const Warframes = require('../models').Warframes;
 const getAll = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     let err, warframes;
-    warframes = [{ Name: 'Ash', Prime: 1, Umbra: 0 },
-    { Name: 'Atlas', Prime: 0, Umbra: 0 },
-    { Name: 'Banshee', Prime: 1, Umbra: 0 },
-    { Name: 'Excalibur', Prime: 1, Umbra: 0 },
-];
+    
+    var whereStatement = {};
+    if (req.query.name)
+        whereStatement.name = {
+            $like: '%' + req.query.name + '%'
+        };
 }
