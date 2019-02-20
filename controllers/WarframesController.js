20f48912e58a093fed31e0ec3f52c2003ba95926
Zyrian Gantuangco <g.zyrian@gmail.com>
2019-02-20 08:01:38 -0500

    Add get function and started creating more global functions

--- controllers/WarframesController.js
+++ controllers/WarframesController.js
@@ -9,4 +9,19 @@ const getAll = (req, res) => {
         whereStatement.name = {
             $like: '%' + req.query.name + '%'
         };
+
+    [err, warframes] = await to(Warframes.findAll());
+
+    let warframesJson = [];
+    for (let i in warframes ) {
+        let warframes = warframes[i];
+    }
 }
+
+const get = (req, res) => {
+    res.setHeader('Content Type', 'application/json');
+    let warframes = req.warframes;
+
+    return ReS(res, {warframes:warframes.toWeb()});
+}
+module.exports.get = get;
--- global_functions.js
+++ global_functions.js
@@ -1,8 +1,24 @@
 pe = require('parse-error'); // parses error so you can read error message and handle them accordingly
 
-to = function (promise) {
+to = function (promise) { // global function that will help us handle promise rejections
   return promise
     .then(data => {
       return [null, data];
     }).catch(err => [pe(err)])
 };
+
+TE = function(err_message, log) { // Throw error
+  if (log === true){
+    console.error(err_message);
+  }
+
+  throw new Error(err_message);
+}
+
+ReE = function(res, err, code) { // Web response error
+  if (typeof err == 'object' && typeof err.message != 'undefined') {
+    err = err.message;
+  }
+
+  if (typeof code !== 'undefinted') res.statusCode = code;
+}
