9f462b087fae0ff5a9e3d1e869c44778649009d1
Zyrian Gantuangco <g.zyrian@gmail.com>
2019-09-27 15:59:05 -0400

    Created users service and component Updated users-list page to pull from db

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UsersService, IUser } from '../users.service';
 
 @Component({
   selector: 'app-users-list',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+  users: any[];
 
-  ngOnInit() {
+  constructor(
+    private usersService: UsersService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.getUsers();
   }
 
+  getUsers(): void {
+    this.usersService.getAll()
+      .subscribe(
+        (success) =>
+          this.users = success,
+        );
+  }
+
+  goToUser(user: IUser): void {
+    this.router.navigate([`/users/${user.id}`]);
+  }
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+
+export interface IUser {
+  id: number;
+  username: string;
+  email: string;
+  isAdmin: boolean;
+  password: string;
+}
+
+export const emptyUser: IUser = {
+  id: 0,
+  username: '',
+  email: '',
+  isAdmin: false,
+  password: ''
+};
 
 @Injectable()
 export class UsersService {
 
-  constructor() { }
+  usersUrl = `http://localhost:5000/users`;
+
+  constructor(private http: HttpClient) { }
+
+  create(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>(`${this.usersUrl}`, user);
+  }
+
+  update(user: IUser): Observable<number[]> {
+    return this.http.put<number[]>(`${this.usersUrl}/${user.id}`, user);
+  }
+
+  getAll(): Observable<IUser[]> {
+    return this.http.get<IUser[]>(`${this.usersUrl}`);
+  }
+
+  get(id: number): Observable<IUser> {
+    return this.http.get<IUser>(`${this.usersUrl}/${id}`);
+  }
 
 }
